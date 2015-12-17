83d3e7116f33c26ff8fd3ec65ab74acef0e8277f
obliviusm <obliviusm@gmail.com>
2015-12-17 12:32:24 -0500

    find template by correlation

--- 2al4/find_template_1D.m
+++ 2al4/find_template_1D.m
@@ -1,13 +1,8 @@
 function index = find_template_1D( t, s )
 %FIND_TEMPLATE_1D Summary of this function goes here
 %   Detailed explanation goes here
-    index = 0;
-    t_length = size(t,2)-1;
-    for i = 1:size(s,2)-t_length
-        if isequal(s(i:i+t_length), t)
-           index = i;
-           break;
-        end
-    end
+    c = normxcorr2(t, s);
+    [maxValue rawIndex] = max(c);
+    index = rawIndex - size(t, 2) + 1;
 end
 
--- 2al4/my_find_template_1D.m
+++ 2al4/my_find_template_1D.m
@@ -2,10 +2,11 @@ function index = find_template_1D( t, s )
 %FIND_TEMPLATE_1D Summary of this function goes here
 %   Detailed explanation goes here
     index = 0;
-    for i = 1:size(s)
-        if isequal(s(i:i+), t)
+    t_length = size(t,2)-1;
+    for i = 1:size(s,2)-t_length
+        if isequal(s(i:i+t_length), t)
            index = i;
-           break
+           break;
         end
     end
 end
