3b57f8a71fb41d51482a11c1b778454a88efe099
obliviusm <obliviusm@gmail.com>
2015-12-17 05:51:02 -0500

    median filtering

---
+++ a2l3median.m
@@ -0,0 +1,14 @@
+% Apply a median filter
+% pkg load image;
+
+%% Read an image
+img = imread('img/bycicle.png');  % also try: brooklyn-bridge.png, penny-farthing.png
+% imshow(img);
+
+%% TODO: Add salt & pepper noise
+img_noise = imnoise(img, 'salt & pepper', 0.02);
+% imshow(img_noise);
+%% TODO: Apply a median filter 
+%  (how is the result different compared to Gaussian smoothing?)
+median_filtered = medfilt2(img_noise);
+imshow(median_filtered);
