62e472b3fb0a6f022e8c67afac5682a86494ab24
obliviusm <obliviusm@gmail.com>
2015-12-17 05:09:45 -0500

    add filtering with boundaries

---
+++ a2l3boundaries.m
@@ -0,0 +1,15 @@
+% Explore edge options
+% pkg load image;
+
+%% Load an image
+img = imread('img/fall-leaves.png');  % also available: peppers.png, mandrill.png
+imshow(img);
+
+%% TODO: Create a Gaussian filter
+hsize = 21;
+sigma = 3;
+filter = fspecial('gaussian', hsize, sigma);
+%% TODO: Apply it, specifying an edge parameter (try different parameters)
+% symmetric, replicate, cicrular, X
+smoothed = imfilter(img, filter, 'circular'); 
+imshow(smoothed);
